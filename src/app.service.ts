2b46be5e53aa4410a9bc635cb94f3c748fda3243
besaklish <108043393+dopenchills@users.noreply.github.com>
2023-06-05 20:57:24 +0900

    guideline: avoid using HTTPException in service

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 
+/**
+ * Guideline: avoid using HTTPException in service.
+ * 
+ * Imagine this service is not directly from controller, but called from another service.
+ * 
+ * `BadRequestException` is not a error that the another service wants to catch 
+ *   because HTTPException is there for the client.
+ */
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(id: string): string {
+    if (id.includes('-')) {
+      throw new BadRequestException('id should not include hyphen');
+    }
+
+    return id;
   }
 }
