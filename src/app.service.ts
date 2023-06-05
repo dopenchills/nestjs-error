80ffc0bde71aa093bf0d379aa28bf17ec24cd539
besaklish <108043393+dopenchills@users.noreply.github.com>
2023-06-05 21:31:04 +0900

    comment: add comment

--- src/app.service.ts
+++ src/app.service.ts
@@ -6,8 +6,11 @@ export class AppService {
    * Guideline: Define return value type to detect wrong try-catch
    * 
    * If I do not define return value type, there is no error from TS.
+   *  Thus, the obviously wrong logic below can be compiled.
    * 
    * Once I define return value type, the program does not compile because of TS.
+   * 
+   *            ↓ this `string` is return value type
    */
   getHello(): string {
     try {
