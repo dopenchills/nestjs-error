e9499563a001c2d657b34d38b459c8bf9ba38e09
besaklish <108043393+dopenchills@users.noreply.github.com>
2023-06-05 21:29:37 +0900

    guideline: Define return value type to detect wrong try-catch

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,9 +3,13 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   /**
-   * Guideline: Avoid blindlessly using try-catch in service
+   * Guideline: Define return value type to detect wrong try-catch
+   * 
+   * If I do not define return value type, there is no error from TS.
+   * 
+   * Once I define return value type, the program does not compile because of TS.
    */
-  getHello() {
+  getHello(): string {
     try {
       throw new Error("something bad happened in service");
     } catch (error) {
