fa11075c3c33929c1cf596aee5b9363fc79ac42e
besaklish <108043393+dopenchills@users.noreply.github.com>
2023-06-05 20:50:39 +0900

    guideline: use try-catch as guard in controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,36 @@
-import { Controller, Get } from '@nestjs/common';
+import { BadRequestException, Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  /**
+   * Guideline: use try-catch as guard in controller
+   */
+  @Get(':id')
+  getHello(@Param('id') id: string): number {
+    if(id.includes('-')) {
+      throw new BadRequestException('id should not include hyphen');
+    }
+
+    return 0;
   }
 }
+
+/**
+ * When calling `http://localhost:3000/-`, server returns as below.
+ * 
+ * {
+    "statusCode": 400,
+    "message": "id should not include hyphen",
+    "error": "Bad Request"
+  }
+ * 
+ * Advantages
+ * 
+ * - The error message is readable to the client.
+ * 
+ * - We can make sure that the id does not include hyphen in the following code.
+ * 
+ */
