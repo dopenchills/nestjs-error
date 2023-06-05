f28c89cb569c86de197263c1792808cbd0b4bce8
besaklish <108043393+dopenchills@users.noreply.github.com>
2023-06-05 20:40:44 +0900

    guideline: Throw HTTPException in controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,33 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, InternalServerErrorException } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  /**
+   * Guideline: Throw HTTPException in controller
+   */
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(): number {
+    try {
+      throw new Error("Something bad happened");
+    } catch (error) {
+      // throw HTTPException
+      throw new InternalServerErrorException("There is a problem in the server side.");
+    }
   }
 }
+
+/**
+ * 
+ * The code above returns below.
+ * 
+ * This is readable for the client side
+ * 
+ * {
+    "statusCode": 500,
+    "message": "There is a problem in the server side.",
+    "error": "Internal Server Error"
+  }
+ */
